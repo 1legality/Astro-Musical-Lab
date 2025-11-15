841918ce8a3a6cf33688d784e761cead8263253e
1legality <5321516+1legality@user.noreply.github.com>
2025-11-15 16:48:36 -0500

    change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://1legality.github.io',
-  base: '/Astro-Musical-Lab',
+  site: 'lab.malandry.com',
   trailingSlash: 'never',
   integrations: [mdx(), react()],
   vite: {
