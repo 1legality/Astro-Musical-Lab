6d254973225f0bddf8529d8a472aba4ea33eba06
1legality <5321516+1legality@user.noreply.github.com>
2025-11-15 16:52:10 -0500

    url fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'lab.malandry.com',
+  site: 'https://lab.malandry.com',
   trailingSlash: 'never',
   integrations: [mdx(), react()],
   vite: {
